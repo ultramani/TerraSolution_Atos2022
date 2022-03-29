5a5c58cd6c4fe574a1a996a3e50ce65aa93a3f15
ultramani <71970337+ultramani@users.noreply.github.com>
2022-03-29 23:40:56 +0200

    Refactored folders structure adn implemented login and register

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask_login import LoginManager
+from .databaseManager import setup_db, db_drop_and_create_all, db
+from .config import APP_CONFIG
+from flask_cors import CORS
+from flask import Flask
+
+
+
+app = Flask(__name__, template_folder='templates')
+#Nos permite cambiar la configuracion con solo cambiar el string
+app.config.from_object(APP_CONFIG["development"])
+#Login manager, manages the login requests
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+#Inicializa la bbdd
+setup_db(app)
+#Añade protecion contra ataques CORS
+CORS(app)
+#Comentar si se quiere persistencia
+db_drop_and_create_all()
+
+from .views import *
+
